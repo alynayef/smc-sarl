ea2ccb75e080b9a23e3a6f9f1e85c5153ea9682e
Basma <aly-nayef.basma@soprasteria.com>
2020-05-10 23:13:45 +0200

    smtp test

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php include_once("index.html") ?>
+<?php include_once("index.html");
+include_once("assets/phpMailer/PHPMailerAutoload.php");  
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = '465';
+$mail->isHTLM();
+$mail->Username = 'alynayef@gmail.com';
+$mail->password = 'Hamete_16';
+$mail->setFrom('no-reply@smc-sarl.com');
+$mail->Body = 'test marchel way';
+$mail->AddAddress('alynayef@gmail');
+$mail->send();
+?>
